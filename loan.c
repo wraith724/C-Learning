c2c917e021a744ec2c694a36f598d523855d1bb9
wraith724 <alvo97030@gmail.com>
2025-09-26 00:35:05 +0300

    Update loan.c

--- loan.c
+++ loan.c
@@ -1,20 +1,28 @@
 /*a program to verify the qualifications
 for receiving a loan
 */
+
 #include<stdio.h>
+
 //main function
 int main(){
+    
     //variable declaration
     float age, income;
+    
     //enter age
     printf("enter your age");
     scanf("%f",&age);
+    
     if(age>=21) printf("enter your annual income");
     else printf("Unfortunately you don't qualify for a loan");
     scanf("%f",&income);
+    
     //input validation
     if (income>=21000) printf("Congratulations you qualify for a loan");
     else printf("Unfortunately you don't qualify for a loan");
+    
     return 0;
 
     }
+
