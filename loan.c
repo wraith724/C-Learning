0e329de3e3ca9d00cc2b57448b4a235c1c68fd43
wraith724 <alvo97030@gmail.com>
2025-09-25 23:40:39 +0300

    Update loan.c

--- loan.c
+++ loan.c
@@ -9,11 +9,12 @@ int main(){
     //enter age
     printf("enter your age");
     scanf("%f",&age);
-    if(age>=27) printf("enter your annual income");
+    if(age>=21) printf("enter your annual income");
     else printf("Unfortunately you don't qualify for a loan");
     scanf("%f",&income);
     //input validation
-    if (income>=30000) printf("Congratulations you qualify for a loan");
+    if (income>=21000) printf("Congratulations you qualify for a loan");
     else printf("Unfortunately you don't qualify for a loan");
     return 0;
+
     }
