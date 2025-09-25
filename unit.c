944a361b59b323265cd077906bed73dcd3671f87
wraith724 <alvo97030@gmail.com>
2025-09-25 22:44:55 +0300

    Add files via upload

---
+++ unit.c
@@ -0,0 +1,14 @@
+//A simple C program
+/*units for computer science year 1 semester 1*/
+#include <stdio.h>
+int main(){
+    printf("communication skills\n");
+    printf("intoduction to computer architecture\n");
+    printf("fundamentals of ICT\n");
+    printf("discrete maths\n");
+    printf("maths for science\n");
+    printf("analogue electronic\n");
+    printf("physics\n");
+    printf("intoduction to programming and problem solving\n");
+    return 0;
+}
