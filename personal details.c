c6c272ba3b7d693a6adaf0a2841342a53b2042fe
wraith724 <alvo97030@gmail.com>
2025-09-26 00:13:36 +0300

    Add files via upload

---
+++ personal details.c
@@ -0,0 +1,18 @@
+/*personal details of user
+*/
+#include<stdio.h>
+//main function
+int main(){
+    int height,phone_number,bank_balance;
+    
+    printf("enter your height");
+    scanf("%d",&height);
+    
+    printf("enter your phone_number");
+    scanf("%d",&phone_number);
+    
+    printf("enter your bank_balance");
+    scanf("%d",&bank_balance);
+    
+    return 0;
+}
