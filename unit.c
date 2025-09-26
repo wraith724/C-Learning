084ee0cebacd40d285838b4e81db67134107e2f4
wraith724 <alvo97030@gmail.com>
2025-09-26 09:40:15 +0300

    Update unit.c

--- unit.c
+++ unit.c
@@ -1,7 +1,13 @@
 //A simple C program
-/*units for computer science year 1 semester 1*/
+/*
+Name:Peter Ngubiru
+Reg No.:CT101/G/26455/25
+Description:a program to show the units for computer science year 1 semester 1
+*/
 #include <stdio.h>
+
 int main(){
+    
     printf("communication skills\n");
     printf("intoduction to computer architecture\n");
     printf("fundamentals of ICT\n");
@@ -10,5 +16,7 @@ int main(){
     printf("analogue electronic\n");
     printf("physics\n");
     printf("intoduction to programming and problem solving\n");
+    
     return 0;
+
 }
